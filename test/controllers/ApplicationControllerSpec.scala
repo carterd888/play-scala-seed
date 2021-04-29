fb3b5351b0a3ad2503c9824f218661de58c3186c
Daniel Carter <carterdaniel@hotmail.com>
2021-04-29 13:51:12 +0100

    added index functionality

--- test/controllers/ApplicationControllerSpec.scala
+++ test/controllers/ApplicationControllerSpec.scala
@@ -1,23 +1,49 @@
 package controllers
 
+import models.DataModel
+import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.mvc.{ControllerComponents}
+import play.api.mvc.ControllerComponents
 import uk.gov.hmrc.play.test.UnitSpec
 import play.api.test.FakeRequest
 import play.api.http.Status
+import repositories.DataRepository
+import org.mockito.ArgumentMatchers._
+import org.mockito.Mockito._
+import play.api.test.Helpers.GET
 
-class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerTest{
+import scala.concurrent.{ExecutionContext, Future}
+
+
+class ApplicationControllerSpec extends UnitSpec with GuiceOneAppPerTest with MockitoSugar{
   lazy val controllerComponents: ControllerComponents = app.injector.instanceOf[ControllerComponents]
 
+  implicit lazy val executionContext: ExecutionContext = app.injector.instanceOf[ExecutionContext]
+  val mockDataRepository: DataRepository = mock[DataRepository]
+
   object TestApplicationController extends ApplicationController(
-    controllerComponents
+    controllerComponents, mockDataRepository, executionContext
+  )
+
+  val dataModel: DataModel = DataModel(
+    "abcd",
+    "test name",
+    "test description",
+    100
   )
 
     "ApplicationController .index" should {
 
-    lazy  val result = TestApplicationController.index()(FakeRequest())
+
+
+
 
       "return TODO" in {
+        when(mockDataRepository.find(any())(any()))
+          .thenReturn(Future(List(dataModel)))
+
+        lazy val result = TestApplicationController.index()(FakeRequest())
+
         status(result) shouldBe Status.OK
       }
     }
